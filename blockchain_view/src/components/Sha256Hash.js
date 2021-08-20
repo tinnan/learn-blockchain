6e1e2eac1dd998e3523c73e9ad0ef4ad64b754c8
Tin N <tin_nan@hotmail.com>
2021-08-21 00:42:23 +0700

    Complete hash function and input/output field

--- blockchain_view/src/components/Sha256Hash.js
+++ blockchain_view/src/components/Sha256Hash.js
@@ -1,16 +1,28 @@
 import { Container, Heading, Input, Text, Textarea, Box, Button } from '@chakra-ui/react'
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { sha256Hash } from '../blockchain/util/hash';
 
 function Sha256Hash() {
+    const [data, setData] = useState("");
+    const [sha256, setSha256] = useState();
+    useEffect(() => {
+        const hashedData = sha256Hash(data);
+        setSha256(hashedData);
+    }, [data])
+
+    function updateData(e) {
+        const formData = e.target.value;
+        setData(formData);
+    }
     return (
         <>
             <Heading mb="10">SHA256 Hash</Heading>
-            <Container>
+            <Container maxW="80%">
                 <Box bg="green.100" padding="6" borderRadius="md">
                     <Text>Data:</Text>
-                    <Textarea bg="white" mb="2" />
+                    <Textarea bg="white" mb="2" onChange={updateData} />
                     <Text>SHA256 print:</Text>
-                    <Input bg="white" mb="2" />
+                    <Input bg="white" mb="2" value={sha256} />
                     <Button colorScheme="blue">Calculate SHA256</Button>
                 </Box>
             </Container>
